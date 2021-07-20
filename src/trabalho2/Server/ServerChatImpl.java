6e17569ef6e9245b76a1cf3c4c6c41111903e5c1
Alisson <aamachado@inf.ufsm.br>
2021-07-20 11:58:54 -0300

    update

---
+++ src/trabalho2/Server/ServerChatImpl.java
@@ -0,0 +1,13 @@
+package trabalho2;
+import java.util.ArrayList;
+
+public class ServerChat implements IServerChat {
+    private ArrayList<String> roomList;
+ 
+    public ArrayList<String> getRooms() {
+        return new ArrayList<String>();
+    }
+    public void createRoom(String roomName){
+
+    }
+}
